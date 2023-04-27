21739338fd50de4ce2aa9d827ea63706c5e5bf81
charbossly <assogbacharbossly@gmail.com>
2023-04-27 20:31:02 +0100

    Day 08 completed

---
+++ Day8/readme.md
@@ -0,0 +1,18 @@
+**DAY8 of #100daysofcode**
+
+**Notions:**
+    -Python Dictionnaries
+    -Dictionaries methods, Nesting
+**Projects**
+    -Exercice Dictionnaries
+
+**Examples :**
+
+
+**Resources**
+
+Here are some resources that was useful:
+    - Python
+   
+
+
---
+++ Day8/test.py
@@ -0,0 +1,16 @@
+thisdict =	{
+  "brand": "Ford",
+  "year": 1964,
+  "model": "Mustang"
+  
+}
+
+dico2 = dict(name = "John", age = 36, country = "Norway")
+
+print(thisdict["year"])
+
+print (dico2["age"])
+
+turpleD = ('arr','ok')
+
+# turpleD[0] = "oui"  not possible because it is a turple
